d6a79a057d236f5869a092a55f5508897f426142
Ive Andric <ive@Mac.fritz.box>
2025-04-12 18:26:34 +0200

    switch way of rendering

--- blocks/landing-page-header/landing-page-header.js
+++ blocks/landing-page-header/landing-page-header.js
@@ -1,50 +1,26 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import { setId } from '../../scripts/helpers.js';
 
 /** @param {HTMLElement} block */
 export default function decorate(block) {
-  const wrapper = document.createElement('div');
+//   const itemsWrapper = document.createElement('div');
+//   itemsWrapper.classList.add('career-two-col-teaser-items');
+  const children = [...block.children];
+    console.log(children);
+  const firstChild = children[0];
+  console.log('firstChild', firstChild);
+  if (firstChild) {
+    setId(firstChild, block);
+  }
 
-  [...block.children].forEach((row) => {
-    const innerDiv = document.createElement('div');
-    moveInstrumentation(row, innerDiv);
-
-    let cardLink = null;
-
-    const firstLink = row.querySelector('a');
-    if (firstLink) {
-      cardLink = document.createElement('a');
-      cardLink.href = firstLink.href;
-    }
-
-    while (row.firstElementChild) innerDiv.append(row.firstElementChild);
-
-    [...innerDiv.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) {
-        div.className = 'hero-image';
-      } else {
-        div.className = 'hero-text';
-      }
-    });
-
-    if (cardLink) {
-      cardLink.append(innerDiv);
-      wrapper.append(cardLink);
-    } else {
-      wrapper.append(innerDiv);
-    }
-  });
-
-  wrapper.querySelectorAll('p').forEach((par) => {
-    par.innerHTML = par.innerText;
-  });
-
-  wrapper.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-
-  block.textContent = '';
-  block.append(wrapper);
+  const remainingChildren = children.slice(1);
+  const content = remainingChildren.slice(0, 3);
+  console.log('content', content);
+  const button = remainingChildren.slice(3, 9);
+  console.log('button', button);
+  const cardImage = remainingChildren.slice(9, 11);
+  console.log('cardImage', cardImage);
+  const backgroundImage = remainingChildren.slice(11, 12);
+  console.log('backgroundImage', backgroundImage);
 }
